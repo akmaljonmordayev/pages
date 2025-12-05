8280bb58a8be13774a5d1ea0cb8a24c08afc63f8
b23064155-art <b23064155@gmail.com>
2025-12-05 14:34:30 +0500

    asdvasdvasdvasdv

--- src/componenets/ProtectedRoute.jsx
+++ src/componenets/ProtectedRoute.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
-
   const token = localStorage.getItem("bilol");
 
   if (!token) {

5f2bcb2c528889e4283be7d104e7b7e929418a3d
procompots <procompot64@gmail.com>
2025-12-05 14:31:26 +0500

    asvdasdvasvd

--- src/componenets/ProtectedRoute.jsx
+++ src/componenets/ProtectedRoute.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("bilol");
-
   if (!token) {
     return <Navigate replace to={"/"} />;
   }
-
   return children;
 }
 
